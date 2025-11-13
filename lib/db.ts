60e9f1d2207bc556e7cb94d97e82bf098c876017
Sourabh Vamdevan <vamdevansourabh@gmail.com>
2025-11-13 12:48:05 +0530

    prisma

---
+++ lib/db.ts
@@ -0,0 +1,17 @@
+
+import { PrismaClient } from '@prisma/client';
+// add  no-var
+
+/* eslint-disable no-var */
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+/* eslint-disable no-var */
+
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prisma = db;
+}
